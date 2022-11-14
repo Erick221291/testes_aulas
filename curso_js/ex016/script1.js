2c10686e6945854996398106af837d6b51425cfe
Erick Marques R <erick.marquesrodrigues7@hotmail.com>
2022-11-14 00:01:01 -0300

    criando contador de numeros

---
+++ curso_js/ex016/script1.js
@@ -0,0 +1,11 @@
+function contar(){
+    let ini = document.getElementById("txti");
+    let fim =  document.getElementById("txtf");
+    let passo = document.getElementById("txtp");
+    
+    if(ini.value.lenght === 0 || fim.value.lenght === 0 || passo.value.lenght === 0){
+      window.alert("[ERRO] faltam dados!!")
+    } else {
+      alert("Tudo OK!")
+    }
+}
